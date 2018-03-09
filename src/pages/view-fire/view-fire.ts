5840a6e9420ae35e8e86628300a901c47a075611
Ray Rommel Santos <ray.santos@engmntprty.com>
2018-03-09 17:48:50 +0800

    gallery looks good

--- src/pages/view-fire/view-fire.ts
+++ src/pages/view-fire/view-fire.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController, Content } from 'ionic-angular';
 
 import { FirebaseProvider } from "../../providers/firebase/firebase";
 
 /**
- * Generated class for the ViewFirePage page.
+ * Generated class for the VisionPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -16,9 +16,16 @@ import { FirebaseProvider } from "../../providers/firebase/firebase";
   templateUrl: 'view-fire.html',
 })
 export class ViewFirePage {
-
-	list : any = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams, private fire: FirebaseProvider) {
+	bgImg: any = '';
+	prod:any;
+	heighter : any = "100vh";
+	dataLoaded = false;
+	tags: Array<any> = [];
+	@ViewChild('vid') vid: ElementRef;
+	@ViewChild(Content) content: Content;
+	floorFont : string = '0.075vh';
+	list: any = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fire: FirebaseProvider, private loader:LoadingController, private render: Renderer2) {
     let self = this;
     fire.getAllImages().then(res=>{
     	for(var propt in res){
@@ -32,6 +39,22 @@ export class ViewFirePage {
 
   ionViewWillEnter() {
     console.log('ionViewDidLoad ViewFirePage');
+  }
+
+  ngAfterViewInit(){
+  	this.heighter = (window.innerHeight)+"px";
+  	this.render.setProperty(this.vid.nativeElement,"muted","muted");
+  	this.render.setProperty(this.vid.nativeElement,"loop","loop");
+  	
+  	setTimeout(()=>{
+	  	this.vid.nativeElement.play().then(console.log).catch(console.info);
+	  	console.log(this.vid.nativeElement);
+  	},1000);
+  }
+
+  scrollingInTheDeep($event){
+  	//console.log($event);
+  	this.content.scrollTo(0,this.heighter.replace(/[^0-9]/g,""),300);
   }
 
 }
